70a5e8fcff04d1a5a690be5126e34e01cbf13c19
rhimm <rhimm_c@outlook.com>
2019-01-17 16:00:20 +0900

    ハードゲージ時のキャラクター・モブの動作を制限

--- TJAPlayer3/Stages/07.Game/Taiko/CAct演奏DrumsMob.cs
+++ TJAPlayer3/Stages/07.Game/Taiko/CAct演奏DrumsMob.cs
@@ -51,7 +51,7 @@ namespace TJAPlayer3
                 //CDTXMania.act文字コンソール.tPrint(0, 0, C文字コンソール.Eフォント種別.白, ctMob.db現在の値.ToString());
                 //CDTXMania.act文字コンソール.tPrint(0, 10, C文字コンソール.Eフォント種別.白, Math.Sin((float)this.ctMob.db現在の値 * (Math.PI / 180)).ToString());
 
-                if(TJAPlayer3.Skin.Game_Mob_Ptn != 0)
+                if(TJAPlayer3.Skin.Game_Mob_Ptn != 0 && TJAPlayer3.ConfigIni.eGaugeMode != EGaugeMode.Hard)
                 {
                     if (TJAPlayer3.stage演奏ドラム画面.actGauge.db現在のゲージ値[0] >= 100)
                     {
