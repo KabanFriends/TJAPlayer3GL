0d66835fec4e55b3c4c6d2e524c04e8cccee55de
rhimm <rhimm_c@outlook.com>
2019-01-20 04:41:34 +0900

    選曲画面での曲読み込み時に読み込んだTJAの文字列によってScrollModeが勝手に切り替わるのを修正

--- TJAPlayer3/Songs/CDTX.cs
+++ TJAPlayer3/Songs/CDTX.cs
@@ -4218,15 +4218,17 @@ namespace TJAPlayer3
         /// <param name="InputText"></param>
         private void t難易度別ヘッダ(string InputText)
         {
-            if (InputText.Equals("#HBSCROLL") && TJAPlayer3.ConfigIni.bスクロールモードを上書き == false)
+            if (TJAPlayer3.actEnumSongs.b活性化してない)
             {
-                TJAPlayer3.ConfigIni.eScrollMode = EScrollMode.HBSCROLL;
+                if (InputText.Equals("#HBSCROLL") && TJAPlayer3.ConfigIni.bスクロールモードを上書き == false)
+                {
+                    TJAPlayer3.ConfigIni.eScrollMode = EScrollMode.HBSCROLL;
+                }
+                if (InputText.Equals("#BMSCROLL") && TJAPlayer3.ConfigIni.bスクロールモードを上書き == false)
+                {
+                    TJAPlayer3.ConfigIni.eScrollMode = EScrollMode.BMSCROLL;
+                }
             }
-            if (InputText.Equals("#BMSCROLL") && TJAPlayer3.ConfigIni.bスクロールモードを上書き == false)
-            {
-                TJAPlayer3.ConfigIni.eScrollMode = EScrollMode.BMSCROLL;
-            }
-
 
             string[] strArray = InputText.Split(new char[] { ':' });
             string strCommandName = "";
