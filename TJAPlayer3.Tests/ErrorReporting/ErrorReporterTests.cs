5029671b1363ba21d2b94cdcfb8ed3ab261f3af1
twopointzero <jeremy@jeremygray.ca>
2020-02-22 15:26:29 -0800

    Error reporting is now fully automatic instead of manual.

---
+++ TJAPlayer3.Tests/ErrorReporting/ErrorReporterTests.cs
@@ -0,0 +1,32 @@
+﻿using NUnit.Framework;
+using TJAPlayer3.ErrorReporting;
+
+namespace TJAPlayer3.Tests.ErrorReporting
+{
+    [TestFixture]
+    public sealed class ErrorReporterTests
+    {
+        [Test]
+        [TestCase("v0.0.0 (unknown informational version)", "development")]
+        [TestCase("4.8.0-update-notification.1+166.Branch.feature-update-notification.Sha.f000e042234cb00e7e03642d21bc0527603b267d", "development")]
+        [TestCase("4.8.0-alpha.167+Branch.develop.Sha.761661a92b7780d680b1b71ac69ba2da5682d904", "alpha")]
+        [TestCase("4.8.0-beta.1+0.Branch.release-4.8.0.Sha.761661a92b7780d680b1b71ac69ba2da5682d904", "beta")]
+        [TestCase("4.8.0+Branch.master.Sha.8ebbb008b20ab366beedb03c53989e42c52cfb97", "production")]
+        public void TestGetEnvironment(string informationalVersion, string environment)
+        {
+            Assert.That(ErrorReporter.GetEnvironment(informationalVersion), Is.EqualTo(environment));
+        }
+
+        [Test]
+        public void TestGetShaFromInformationalVersion()
+        {
+            const string informationalVersion =
+                "5.0.0-automatic-error-reporting.1+173.Branch.feature-automatic-error-reporting.Sha.5e794d5329511ac26c2e11dc05da806fd33c2661";
+            const string expected = "5e794d5329511ac26c2e11dc05da806fd33c2661";
+
+            var actual = ErrorReporter.GetShaFromInformationalVersion(informationalVersion);
+
+            Assert.That(actual, Is.EqualTo(expected));
+        }
+    }
+}
---
+++ TJAPlayer3/ErrorReporting/ErrorReporter.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Diagnostics;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Rollbar;
+
+namespace TJAPlayer3.ErrorReporting
+{
+    public static class ErrorReporter
+    {
+        private const string EnvironmentAlpha = "alpha";
+        private const string EnvironmentBeta = "beta";
+        private const string EnvironmentDevelopment = "development";
+        private const string EnvironmentProduction = "production";
+
+        public static void WithErrorReporting(Action action)
+        {
+            try
+            {
+                var appInformationalVersion = TJAPlayer3.AppInformationalVersion;
+
+                var environment = GetEnvironment(appInformationalVersion);
+                var codeVersion = environment == EnvironmentDevelopment
+                    ? null
+                    : GetShaFromInformationalVersion(appInformationalVersion);
+
+                var rollbarConfig = new RollbarConfig("a4c98d82d6534bdab3fd9583029314e0")
+                {
+                    CaptureUncaughtExceptions = true,
+                    Environment = environment,
+                    RethrowExceptionsAfterReporting = false,
+                    Transform = payload =>
+                    {
+                        if (codeVersion != null)
+                        {
+                            payload.Data.CodeVersion = codeVersion;
+                        }
+                        payload.Data.Platform = "client";
+                    }
+                };
+                RollbarLocator.RollbarInstance.Configure(rollbarConfig);
+
+                RollbarLocator.RollbarInstance.AsBlockingLogger(TimeSpan.FromSeconds(10))
+                    .Info("Temporary Environment & Code Version test info-level event.");
+
+                Application.ThreadException += (sender, args) =>
+                {
+                    ReportError(args.Exception);
+                };
+
+                AppDomain.CurrentDomain.UnhandledException += (sender, args) =>
+                {
+                    ReportError(args.ExceptionObject as Exception);
+                };
+
+                TaskScheduler.UnobservedTaskException += (sender, args) =>
+                {
+                    ReportError(args.Exception);
+                };
+
+                action();
+            }
+            catch (Exception e)
+            {
+                ReportError(e);
+            }
+        }
+
+        private static void ReportError(Exception e)
+        {
+#if !DEBUG
+            try
+            {
+                Trace.WriteLine("");
+                Trace.WriteLine(e);
+                Trace.WriteLine("");
+                Trace.WriteLine("エラーだゴメン！（涙");
+
+                RollbarLocator.RollbarInstance.AsBlockingLogger(TimeSpan.FromSeconds(5)).Error(e);
+            }
+            catch (TimeoutException)
+            {
+                Trace.WriteLine("Timeout encountered when attempting to report an error to Rollbar");
+            }
+            catch (Exception exception)
+            {
+                Trace.WriteLine("Unexpected exception encountered when attempting to report an error: " + exception);
+            }
+#endif
+        }
+
+        public static string GetEnvironment(string informationalVersion)
+        {
+            switch (Regex.Match(informationalVersion, @"(?<=^.+?[+-])\w+").Value)
+            {
+                case "Branch":
+                {
+                    return EnvironmentProduction;
+                }
+                case "beta":
+                {
+                    return EnvironmentBeta;
+                }
+                case "alpha":
+                {
+                    return EnvironmentAlpha;
+                }
+                default:
+                {
+                    return EnvironmentDevelopment;
+                }
+            }
+        }
+
+        public static string GetShaFromInformationalVersion(string informationalVersion)
+        {
+            return Regex.Match(informationalVersion, @"(?<=\.)[0-9a-f]{40}$").Value;
+        }
+    }
+}
